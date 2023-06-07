28c17f88adcf0c21223fbce743c906bc0f0af4d7
alexgim <alexgim961101@gmail.com>
2023-06-07 16:08:53 +0900

    [02] 회원가입, 로그인, 로그아웃 기능 완료

--- blog/src/main/java/com/example/blog/controller/UserController.java
+++ blog/src/main/java/com/example/blog/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.example.blog.controller;
 
-import com.example.blog.dto.LoginDto;
+import com.example.blog.dto.LoginForm;
 import com.example.blog.entity.member.Member;
 import com.example.blog.entity.member.MemberRepository;
 import com.example.blog.service.UserService;
@@ -8,6 +8,7 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -16,13 +17,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.Optional;
-
 @Controller
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
     private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @GetMapping("/")
     public String homePage(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
@@ -36,15 +36,15 @@ public class UserController {
     }
 
     @GetMapping("/login")
-    public String loginPage(@ModelAttribute LoginDto loginDto) {
+    public String loginPage(@ModelAttribute LoginForm loginForm) {
         return "member/loginForm";
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginDto loginDto, BindingResult bindingResult, HttpServletResponse response) {
+    public String login(@Valid @ModelAttribute LoginForm loginForm, BindingResult bindingResult, HttpServletResponse response) {
         if(bindingResult.hasErrors()) return "member/loginForm";
 
-        Member loginMember = userService.login(loginDto.getLoginId(), loginDto.getPassword());
+        Member loginMember = userService.login(loginForm.getLoginId(), loginForm.getPassword());
 
         if(loginMember == null) {
             bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
@@ -77,6 +77,7 @@ public class UserController {
             return "member/addMemberForm";
         }
 
+        member.setPassword(bCryptPasswordEncoder.encode(member.getPassword()));
         memberRepository.save(member);
         return "redirect:/";
     }
--- blog/src/main/java/com/example/blog/dto/LoginForm.java
+++ blog/src/main/java/com/example/blog/dto/LoginForm.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
-public class LoginDto {
+public class LoginForm {
 
     @NotEmpty
     private String loginId;
